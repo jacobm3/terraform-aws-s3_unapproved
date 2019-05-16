20d214528ec0c5ec3b264b4a7e32da772b88a5b7
Jacob <jacobm3@gmail.com>
2019-05-15 23:16:01 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+    type = "string"
+    description = "Name of the bucket. Must be between 3 and 63 characters long, and can contain only lower-case characters, numbers, periods, and dashes."
+}
